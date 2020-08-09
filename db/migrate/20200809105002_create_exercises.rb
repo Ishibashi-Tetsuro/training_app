b81d56ccd8272bf51cd8f4b1896c0542b355d87c
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-09 19:58:25 +0900

    エクササイズテーブルの追加

--- db/migrate/20200809105002_create_exercises.rb
+++ db/migrate/20200809105002_create_exercises.rb
@@ -1,7 +1,9 @@
 class CreateExercises < ActiveRecord::Migration[6.0]
   def change
     create_table :exercises do |t|
-
+      t.string :part
+      t.text :url
+      t.integer :level
       t.timestamps
     end
   end
