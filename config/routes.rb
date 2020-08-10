3ae8bd97c68dd130b74e7e0d1ac3b54ce02ffe30
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-10 23:25:18 +0900

    feat-MVC showアクションのMVCの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'exercises#index'
-  get 'exercises/index'
   devise_for :users
+  resources :exercises
+  root "exercises#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
