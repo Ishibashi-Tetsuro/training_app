335282c119b63f0a8fa969e618ec17f897297e7e
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-11 00:35:21 +0900

    chore/migrate diaryモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_105002) do
+ActiveRecord::Schema.define(version: 2020_08_10_153225) do
+
+  create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
 
   create_table "exercises", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "part"
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_08_09_105002) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diaries", "users"
 end
