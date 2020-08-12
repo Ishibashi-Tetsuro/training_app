c597532823a434101c40e43694b4ebf7c359ddb4
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-12 15:28:47 +0900

    update:エクササイズ投稿機能の実装

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,7 +4,24 @@ class ExercisesController < ApplicationController
   end
 
   def show
+    @exercise = Exercise.new
     @schedules = Schedule.where(user_id: current_user.id, training_date: Date.today)
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    binding.pry
+    if @exercise.save
+      redirect_to exercise_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:url, :part, :level)
   end
 
 end
