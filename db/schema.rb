5ad8aba551fed67884c9f1861fabb5874c819561
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-12 16:40:50 +0900

    chore:exercisesテーブルへuser_idカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_024423) do
+ActiveRecord::Schema.define(version: 2020_08_12_073524) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_024423) do
     t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
