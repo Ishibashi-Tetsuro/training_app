78e50ec956e9dbed590b47774ee0284af320ed3a
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-14 19:24:38 +0900

    update:新規投稿のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   resources :exercises, only:[:new, :create, :show, :index]
-  resources :diaries, only: [:index, :show]
+  resources :diaries, only: [:index, :show, :new, :create]
   resources :users, only: [:show]
   resources :schedules, only: [:new, :create, :index]
   root "exercises#index"
