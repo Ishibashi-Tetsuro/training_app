0b06aa90d86bc02e654ca620850a82f3962ee860
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-14 19:25:01 +0900

    update:新規投稿のコントローラー設定

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,5 @@
 class DiariesController < ApplicationController
+
   def index
     @diaries = Diary.all
   end
@@ -7,6 +8,23 @@ class DiariesController < ApplicationController
     @diary = Diary.find(params[:id])
   end
 
-  def create
+  def new
+    @diary = Diary.new
   end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
