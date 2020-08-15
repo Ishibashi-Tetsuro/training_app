48a1f94b14188aa5d25f5d6ab993ef48c9b09b62
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-15 12:33:40 +0900

    Fix:誤字の訂正

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,16 @@
 class Diary < ApplicationRecord
+
+  attr_accessor :remove_image
+
+  before_save :remove_image_if_user_accept
+
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image, dependent: false
+
+  private
+
+  def remove_image_if_user_accept
+    self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+  end
+
 end
