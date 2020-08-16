80f79eba476b30a127cc41ad355e30c99f599ace
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-17 00:30:42 +0900

    Chore:imageカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_081633) do
+ActiveRecord::Schema.define(version: 2020_08_16_152907) do
 
   create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
