4c4d0ba778e4a7e8b6eec8601a7e5e4705bdef88
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-19 20:23:33 +0900

    Chore:CharacterModelの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_111554) do
+ActiveRecord::Schema.define(version: 2020_08_19_112239) do
+
+  create_table "characters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "counts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "day"
