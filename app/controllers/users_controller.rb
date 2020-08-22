7d4a33b5abd2b22dd225ef9597bd19f20217526a
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-22 17:16:04 +0900

    refactor:変数の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @diaries = @user.diaries
-    @schedules = Schedule.where(user_id: current_user.id).order(training_date: :asc)
-    @exercises = Exercise.where(user_id: current_user.id)
+    @diaries = Diary.where(user_id: @user.id)
+    @schedules = Schedule.where(user_id: @user.id).order(training_date: :asc)
+    @exercises = Exercise.where(user_id: @user.id)
   end
 end
