06877ebb0e425909975b66dd8bd5488fb4d7e7a1
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-22 15:22:35 +0900

    Refactor:リファクター

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,11 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: "テストユーザーとしてログインしました"
+  end
   # GET /resource/sign_in
   # def new
   #   super
@@ -20,15 +24,11 @@ class Users::SessionsController < Devise::SessionsController
 
   # protected
 
+  # def update_resource(resource, params)
+  #   resource.update_without_password(params)
+  # end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-
-  def new_guest
-    user = User.guest
-    sign_in user
-    redirect_to root_path, notice: "テストユーザーとしてログインしました"
-  end
-
 end
