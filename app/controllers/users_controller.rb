d0657c548001aff2626381549bde31c41617fcd2
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-22 17:48:30 +0900

    delete:ユーザー詳細ページに非ログインユーザーのアクセスも許可

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-
   def show
     @user = User.find(params[:id])
     @diaries = Diary.where(user_id: @user.id)
