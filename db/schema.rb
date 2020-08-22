ac1e092867692d113ae9558efe85be208a684eb4
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-22 11:13:17 +0900

    Add:Usersテーブルへカラムの追加(name,image)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_070022) do
+ActiveRecord::Schema.define(version: 2020_08_22_021204) do
 
   create_table "characters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "image"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_070022) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
