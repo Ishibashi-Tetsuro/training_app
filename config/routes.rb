dc2fba0ce972b74c1bea91c4975240b7df315658
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-26 15:30:40 +0900

    Update:投稿削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   resources :users, only: [:show]
-  resources :diaries, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :diaries, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :likes, only: [:create, :destroy]
   end
   resources :exercises, only:[:new, :create, :show, :index]
