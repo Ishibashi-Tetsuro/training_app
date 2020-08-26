73045a38c8c423e902cfd84fac63cc826bf3d970
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-26 15:31:11 +0900

    fix:リクエストテストの途中

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   image_path = Rails.root.join("app/assets/images/default.png")
 
   factory :diary do
-    content {  'abc' }
+    content {  Faker::Lorem.sentence(10) }
     image { File.open(image_path) }
     association :user
   end
