d21faa190b94942818af4870473006e8c784604b
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-27 22:38:04 +0900

    Chore:JQueryの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+
+console.log('hoge')
 
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
