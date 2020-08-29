00a0f68c31a12cf6e7b787f81dd621b864256ddd
mohupor03 <mohupor03@yahoo.co.jp>
2020-08-29 15:57:33 +0900

    てすと

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module TrainingApp
     config.active_storage.variant_processor = :vips
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
