5abab33653a1de985356240bffe85794748ee76e
mohupor03 <mohupor03@yahoo.co.jp>
2020-09-11 22:10:29 +0900

    Update:いいね！のAjax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(diary_id: params[:diary_id])
-    redirect_back(fallback_location: root_path)
+    @like = current_user.likes.new(diary_id: params[:diary_id])
+    @like.save
+    # @like = current_user.likes.create(diary_id: params[:diary_id])
+    # redirect_back(fallback_location: root_path)
+    @diary = Diary.find(params[:diary_id])
+    respond_to :js
   end
 
   def destroy
     @like = Like.find_by(diary_id: params[:diary_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+    @diary = Diary.find(params[:diary_id])
+    respond_to :js
   end
 end
